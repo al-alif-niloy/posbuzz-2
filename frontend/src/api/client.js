fe2331be9089022fe5d84ccb26456cdb65095686
al-alif-niloy <niloy.alif21@gmail.com>
2026-01-28 20:05:17 +0600

    Prepare frontend for live deployment

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,18 +1,18 @@
-
-
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
-
+  baseURL: import.meta.env.VITE_API_BASE_URL, // use live backend
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-})
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
